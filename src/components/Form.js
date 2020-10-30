56015a8ba2cccca8f9b346abfe7a39ea655fbe06
lauravikanis <lauravikanis@yahoo.de>
2020-10-30 16:06:42 +0100

    :bug: fix date and inputvalue

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 export default function Form({ setTuduObject }) {
+  const [inputValue, setInputValue] = useState("");
+
   const getDate = () => {
     const time = new Date();
     const day = time.getDate();
-    const month = time.getMonth();
+    const month = time.getMonth() + 1;
     const year = time.getFullYear();
     return `${day}|${month}|${year}`;
   };
@@ -11,14 +15,15 @@ export default function Form({ setTuduObject }) {
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        const input = document.querySelector(".todo__input"); // ToDo: Verschlanken?
         setTuduObject({
-          text: input.value,
+          text: inputValue,
           date: getDate(),
         });
       }}
     >
       <input
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
         type="text"
         placeholder="Was willst du dun?"
         className="todo__input"
