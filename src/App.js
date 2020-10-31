15d6c9a5122603975c6e29fe428171a206b18ea6
bwnstk <info@flimmerkiste.xyz>
2020-10-31 13:27:49 +0100

    :sparkles: Add chooseOneRandom out of array

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import "./global.css";
 function App() {
   const [values, setValues] = useState([]);
 
+  function choosOneRandom(array) {
+    const randNrInArray = Math.floor(Math.random() * array.length);
+    console.log({ randNrInArray });
+    return array[randNrInArray];
+  }
+
+  console.log("i choose:", choosOneRandom(values));
+
   return (
     <div className="container">
       <Header />
