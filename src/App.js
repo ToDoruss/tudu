d54cfa9d561ca196328a5d663db98c1f6a04ed15
bwnstk <info@flimmerkiste.xyz>
2020-10-31 14:45:07 +0100

    Add the Lucky one

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { choosOneRandom } from "./utils/helpers";
 function App() {
   const [values, setValues] = useState([]);
   const [alreadyChoosen, setAlredyChoosen] = useState([]);
+  const [theLuckyOne, setTheLuckyOne] = useState("");
 
   const moveFromValuesToAlreadyChoosen = (value) => {
     const valuesCleaned = values.filter((item) => item !== value);
@@ -21,22 +22,46 @@ function App() {
       <Header />
       <main className="main">
         <Form setValues={setValues} setAlreadyChoosen={setAlredyChoosen} />
+
         <div className="realDecide">
           <h2>wheelTitel</h2>
-          <div className="valuesToChoose">
-            {values.length > 0 && <RealDecide values={values} />}
-          </div>
           <button
             className="button__reRun"
-            onClick={() =>
-              moveFromValuesToAlreadyChoosen(choosOneRandom(values))
-            }
+            onClick={() => {
+              if (values.length > 0) {
+                const randomPerson = choosOneRandom(values);
+                setTheLuckyOne(randomPerson);
+                moveFromValuesToAlreadyChoosen(randomPerson);
+              } else {
+                setValues(alreadyChoosen);
+                setTheLuckyOne(null);
+                setAlredyChoosen([]);
+              }
+            }}
           >
             ReRun
           </button>
+          {theLuckyOne && (
+            <>
+              <h2>Its you!</h2>
+              <h3>{theLuckyOne}</h3>
+            </>
+          )}
+          <div className="valuesToChoose">
+            {values.length > 0 && (
+              <>
+                <h2>Participants</h2>
+                <RealDecide values={values} />
+              </>
+            )}
+          </div>
+
           <div className="alreadyChoosen">
             {alreadyChoosen.length > 0 && (
-              <RealDecide values={alreadyChoosen} />
+              <>
+                <h2>Already Choosen</h2>
+                <RealDecide values={alreadyChoosen} />
+              </>
             )}
           </div>
         </div>
